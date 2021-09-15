df5af7e5b5cc9c958d53e11a9e1d9fb2341afcbc
Your Name <you@example.com>
2021-09-14 22:40:56 -0500

    edit to roles function

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,19 +1,17 @@
 module.exports = {
     name: "interactionCreate",
     execute(interaction, AssignRoles) {
-        console.log(JSON.stringify(interaction));
         const member = interaction.member;
         if (interaction.customId === "GA") {
             const user = interaction.message.interaction.user.id;
-            console.log(interaction.GuildMember);
             const role = "885696925838479460";
             if (member.roles.cache.some((role) => role.name === "GA STUDENT")) {
                 member.roles.remove(role);
-                interaction.reply(`${interaction.user.tag} Your role has been removed`);
+                interaction.update({ content: `${interaction.user.tag} Your role has been removed` });
                 return;
             }
             member.roles.add(role);
-            interaction.reply(`GA Role Given To ${interaction.user.tag}`);
+            interaction.update({ content: `GA Role Given To ${interaction.user.tag}` });
         }
     },
 };
