19f68da62437ff805664092e779a42a1471327de
Your Name <you@example.com>
2021-09-21 17:56:47 -0500

    added the helper role

--- commands/roles.js
+++ commands/roles.js
@@ -6,9 +6,6 @@ module.exports = {
     data: new SlashCommandBuilder().setName("roles").setDescription("Replies with roles to pick"),
     /**
      *        .addUserOption((option) => option.setName("target").setDescription("to show the targeted user's tag"))
-     *
-     *
-     *
      */
     async execute(interaction) {
         const buttonEmb = new MessageEmbed()
@@ -16,9 +13,12 @@ module.exports = {
             .setTitle("Pick Your Roles!")
             .setAuthor(interaction.user.tag);
 
-        const buttons = new MessageActionRow().addComponents(
-            new MessageButton().setCustomId("GA").setLabel("GA STUDENT").setStyle("DANGER"),
+        const ga_role = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId("GA").setLabel("GA Student").setStyle("DANGER"),
         );
-        interaction.reply({ embeds: [buttonEmb], components: [buttons] });
+        const helper_role = new MessageActionRow().addComponents(
+            new MessageButton().setCustomId("CH").setLabel("Code Helper").setStyle("SUCCESS"),
+        );
+        interaction.reply({ embeds: [buttonEmb], components: [ga_role, helper_role] });
     },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,13 +5,24 @@ module.exports = {
         if (interaction.customId === "GA") {
             const user = interaction.message.interaction.user.id;
             const role = "885696925838479460";
-            if (member.roles.cache.some((role) => role.name === "GA STUDENT")) {
+            if (member.roles.cache.some((role) => role.name === "GA Student")) {
                 member.roles.remove(role);
                 interaction.update({ content: `${interaction.user.tag} Your role has been removed` });
                 return;
             }
             member.roles.add(role);
             interaction.update({ content: `GA Role Given To ${interaction.user.tag}` });
+        }
+        if (interaction.customId === "CH") {
+            const user = interaction.message.interaction.user.id;
+            const role = "889996083772686367";
+            if (member.roles.cache.some((role) => role.name === "Code Helper")) {
+                member.roles.remove(role);
+                interaction.update({ content: `${interaction.user.tag} Your role has been removed` });
+                return;
+            }
+            member.roles.add(role);
+            interaction.update({ content: `Code Helper Role Given To ${interaction.user.tag}` });
         }
     },
 };
